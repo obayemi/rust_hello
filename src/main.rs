3df389fb7992cf332d9ce66de2bdb547e336ca4d
obayemi <obayemi@ikjpz.com>
2022-02-21 23:49:46 +0100

    db integration (#7)
    * db integration

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 #[macro_use]
 extern crate rocket;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::postgres::PgPool;
+use dotenv::dotenv;
+use std::env;
+use rocket::serde::{Serialize, json::Json};
+
+
+#[derive(Serialize)]
+struct Rule {
+    name: String,
+    rule: String,
+    extra: String,
+}
 
 #[get("/hello/<name>/<age>")]
 fn hello(name: &str, age: u8) -> String {
@@ -16,12 +29,43 @@ fn index() -> String {
     "hellllllllllllo wodl ‽".to_string()
 }
 
+#[get("/rules")]
+async fn rules_list(pool: &rocket::State<PgPool>) -> Json<Vec<Rule>> {
+    sqlx::query_as!(Rule, "SELECT name, rule, extra FROM rules").fetch_all(&**pool).await.unwrap().into()
+}
+
+#[get("/rules/<name>")]
+async fn rules_detail(name: String, pool: &rocket::State<PgPool>) -> Option<Json<Rule>> {
+    Some(sqlx::query_as!(Rule, "SELECT name, rule, extra FROM rules WHERE name = $1", name).fetch_optional(&**pool).await.unwrap()?.into())
+}
+
 #[get("/ok")]
 fn healthcheck() -> String {
     "ok".to_string()
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello, index, hello_kube, healthcheck])
+#[rocket::main]
+async fn main() {
+    dotenv().ok();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&env::var("DATABASE_URL").expect("missing `DATABASE_URL` env variable"))
+        .await
+        .expect("error connecting to the db");
+
+    rocket::build()
+        .mount(
+            "/",
+            routes![
+            hello,
+            index,
+            hello_kube,
+            healthcheck,
+            rules_list,
+            rules_detail,
+            ])
+        .manage(pool)
+        .launch()
+        .await
+        .unwrap();
 }
